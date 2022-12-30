8682ec67f83225db8884991c695c99f633989759
adelelsharawy <121696105+adelelsharawy@users.noreply.github.com>
2022-12-30 23:55:42 +0200

    Add files via upload

---
+++ perfect numper.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int firstnumber, scondnumber, sum = 0;
+
+Console.WriteLine("Please enter the first number1");
+firstnumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter the scond number2");
+scondnumber  =int.Parse(Console.ReadLine());
+
+Console.WriteLine("\n The prefect numbers between {0} and {1} is :\n", firstnumber, scondnumber);
+
+for (int x = firstnumber; x <= scondnumber; x++)
+{
+    sum = 0;
+    for (int y = 1; y < x; y++)
+    {
+        if (x % y == 0)
+        {
+            sum += y; 
+        }
+    }
+    if (sum == x && x != 0)
+        Console.WriteLine(x);
+}
+
+
+
+
+
+
---
+++ prime.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int firstNumber, scondNumber, sum = 0;
+
+
+Console.WriteLine("enter the first number");
+firstNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("enter the scond number");
+scondNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine("\n The prime numbers betwen {0} and {1} is : \n", firstNumber, scondNumber);
+
+
+for (int a = firstNumber; a <= scondNumber; a++)
+{
+    sum = 0;
+    for (int b = 2; b <= a / 2; b++)
+    {
+        if (a % b == 0)
+        {
+            sum = sum + 1;
+            break;
+        }
+    }
+    if (sum == 0 && a != 1 && a != 0)
+    {
+        Console.WriteLine("\t {0}", a);
+    }
+}
+
+
+
+
+
+
+
+
